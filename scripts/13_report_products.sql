737d9fdb923a97fffedd7f0b5eea35794da8afb2
SHALINI CHATURVEDY <154950054+ChaturvedyShalini@users.noreply.github.com>
2026-04-08 23:23:07 +0530

    Update
    Removed the final query that combines all product results into one output.

--- scripts/13_report_products.sql
+++ scripts/13_report_products.sql
@@ -77,38 +77,3 @@ GROUP BY
     cost
 )
 
-/*---------------------------------------------------------------------------
-  3) Final Query: Combines all product results into one output
----------------------------------------------------------------------------*/
-SELECT 
-	product_key,
-	product_name,
-	category,
-	subcategory,
-	cost,
-	last_sale_date,
-	DATEDIFF(MONTH, last_sale_date, GETDATE()) AS recency_in_months,
-	CASE
-		WHEN total_sales > 50000 THEN 'High-Performer'
-		WHEN total_sales >= 10000 THEN 'Mid-Range'
-		ELSE 'Low-Performer'
-	END AS product_segment,
-	lifespan,
-	total_orders,
-	total_sales,
-	total_quantity,
-	total_customers,
-	avg_selling_price,
-	-- Average Order Revenue (AOR)
-	CASE 
-		WHEN total_orders = 0 THEN 0
-		ELSE total_sales / total_orders
-	END AS avg_order_revenue,
-
-	-- Average Monthly Revenue
-	CASE
-		WHEN lifespan = 0 THEN total_sales
-		ELSE total_sales / lifespan
-	END AS avg_monthly_revenue
-
-FROM product_aggregations 
